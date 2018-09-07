b52c57eccfddb1fafea8d723e43bb335be185dbf
Angelos Lechouritis <angelos.lexouritis@gmail.com>
2018-09-07 23:19:57 +0300

    toast added for android

--- android/src/main/java/com/reactlibrary/RNSmtpMailerModule.java
+++ android/src/main/java/com/reactlibrary/RNSmtpMailerModule.java
@@ -5,10 +5,13 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
 
 public class RNSmtpMailerModule extends ReactContextBaseJavaModule {
-
+  
   private final ReactApplicationContext reactContext;
+  private static final String DURATION_SHORT_KEY = "SHORT";
+  private static final String DURATION_LONG_KEY = "LONG";
 
   public RNSmtpMailerModule(ReactApplicationContext reactContext) {
     super(reactContext);
@@ -18,5 +21,25 @@ public class RNSmtpMailerModule extends ReactContextBaseJavaModule {
   @Override
   public String getName() {
     return "RNSmtpMailer";
+  }
+
+  @Override
+    public Map<String, Object> getConstants() {
+        // Export any constants to be used in your native module
+      final Map<String, Object> constants = new HashMap<>();
+        constants.put(DURATION_SHORT_KEY, Toast.LENGTH_SHORT);
+        constants.put(DURATION_LONG_KEY, Toast.LENGTH_LONG);
+        return constants;
+    }
+
+  @ReactMethod
+  public void show(String text, int duration, final Promise promise){
+    try{
+     Toast.makeText(ReactApplicationContext, text, duration).show()
+     promise.resolve(true)
+    }
+    catch(Exception e){
+      promise.reject("ERR_UNEXPECTED_EXCEPTION",e)
+    }
   }
 }
