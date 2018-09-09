b7e1c98980c629152084cf2357842a15f32e9a0c
Angelos Lechouritis <angelos.lexouritis@gmail.com>
2018-09-09 18:55:29 +0300

    android_implemented

--- example/App.js
+++ example/App.js
@@ -9,13 +9,36 @@
 import React, { Component } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 import RNSmtpMailer from "react-native-smtp-mailer";
+import RNFS from "react-native-fs";
 
 export default class App extends Component {
-  test = () => {
+  toastShow = () => {
     RNSmtpMailer.show("dummy message", RNSmtpMailer.SHORT)
       .then(val => {
         console.log(val);
       })
+      .catch(err => console.log(err));
+  };
+  sendEmail = () => {
+    RNSmtpMailer.sendMail({
+      mailhost: "smtp.gmail.com",
+      port: "465",
+      ssl: true, //if ssl: false, TLS is enabled
+      username: "usernameEmail",
+      password: "password",
+      from: "fromEmail",
+      recipients: "toEmail1,toEmail2",
+      subject: "subject",
+      htmlBody: "<h1>header</h1><p>body</p>",
+      attachmentPaths: [
+        RNFS.ExternalDirectoryPath + "/image.jpg",
+        RNFS.DocumentDirectoryPath + "/test.txt",
+        RNFS.DocumentDirectoryPath + "/test2.csv"
+      ],
+      attachmentNames: ["image.jpg", "firstFile.txt", "secondFile.csv"],
+      attachmentTypes: ["img", "txt", "csv"]
+    })
+      .then(success => console.log(success))
       .catch(err => console.log(err));
   };
 
@@ -23,10 +46,7 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to Smtp Mailer!</Text>
-        <Text style={styles.instructions}>
-          To get started, press fllowing button
-        </Text>
-        <Button title="Press me" onPress={this.test} />
+        <Button title="Send Email" onPress={this.sendEmail} />
       </View>
     );
   }
