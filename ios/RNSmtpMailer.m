1171d51fc2b44edb5e326b2d86194c038a96a314
Angelos Lechouritis <angelos.lexouritis@gmail.com>
2018-09-09 20:21:23 +0300

    pass of obj to obj c

--- ios/RNSmtpMailer.m
+++ ios/RNSmtpMailer.m
@@ -7,9 +7,23 @@
 {
     return dispatch_get_main_queue();
 }
-RCT_EXPORT_MODULE(sendEmail:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXPORT_MODULE(sendEmail:(NSDictionary *)obj ,sendEmailWithResolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
-    if (true) {
+    NSString *mailhost = [RCTConvert NSString:obj[@"mailhost"]];
+    NSString *port = [RCTConvert NSString:obj[@"port"]];
+    NSString *username = [RCTConvert NSString:obj[@"username"]];
+    NSString *password = [RCTConvert NSString:obj[@"password"]];
+    NSString *from = [RCTConvert NSString:obj[@"from"]];
+    NSString *recipients = [RCTConvert NSString:obj[@"recipients"]];
+    NSString *subject = [RCTConvert NSString:obj[@"subject"]];
+    NSString *body = [RCTConvert NSString:obj[@"htmlBody"]];
+    BOOL ssl = [RCTConvert BOOL:obj[@"ssl"]];
+    NSArray *attachmentPaths = [RCTConvert NSArray:obj[@"attachmentPaths"]];
+    NSArray *attachmentNames = [RCTConvert NSArray:obj[@"attachmentNames"]];
+    NSArray *attachmentTypes = [RCTConvert NSArray:obj[@"attachmentTypes"]];
+
+    RCTLogInfo(@"start sending email!");
+    if (ssl) {
         resolve(@(YES));
     } else {
         resolve(@(NO));
