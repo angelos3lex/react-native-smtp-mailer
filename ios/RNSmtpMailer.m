6c9726e90314469ca1484381e117e776c6be71d7
Angelos Lech <angelos@172.16.103.129>
2020-11-21 13:45:45 -0800

    fix optional ios params

--- ios/RNSmtpMailer.m
+++ ios/RNSmtpMailer.m
@@ -16,12 +16,18 @@ RCT_EXPORT_METHOD(sendMail:(NSDictionary *)obj resolver:(RCTPromiseResolveBlock)
     NSString *port = [RCTConvert NSString:obj[@"port"]];
     NSString *username = [RCTConvert NSString:obj[@"username"]];
     NSString *password = [RCTConvert NSString:obj[@"password"]];
-    NSString *fromName = [RCTConvert NSString:obj[@"fromName"]];
-    NSString *replyToAddress = [RCTConvert NSString:obj[@"replyTo"]];
     NSString *recipients = [RCTConvert NSString:obj[@"recipients"]];
-    NSArray *bcc = [RCTConvert NSArray:obj[@"bcc"]];
     NSString *subject = [RCTConvert NSString:obj[@"subject"]];
     NSString *body = [RCTConvert NSString:obj[@"htmlBody"]];
+    
+    NSString *fromName = [RCTConvert NSString:obj[@"fromName"]];
+    
+    NSString *replyToAddress = [RCTConvert NSString:obj[@"replyTo"]];
+    if (replyToAddress == nil) {
+        replyToAddress = username;
+    }
+    
+    NSArray *bcc = [RCTConvert NSArray:obj[@"bcc"]];
     NSArray *attachmentPaths = [RCTConvert NSArray:obj[@"attachmentPaths"]];
     
     NSNumber *portNumber = [NSNumber numberWithLongLong:port.longLongValue];
@@ -35,6 +41,7 @@ RCT_EXPORT_METHOD(sendMail:(NSDictionary *)obj resolver:(RCTPromiseResolveBlock)
     smtpSession.authType = MCOAuthTypeSASLPlain;
     smtpSession.connectionType = MCOConnectionTypeTLS;
     MCOMessageBuilder *builder = [[MCOMessageBuilder alloc] init];
+    
     MCOAddress *from = [MCOAddress addressWithDisplayName:fromName
                                                 mailbox:username];
                                                 
@@ -43,38 +50,41 @@ RCT_EXPORT_METHOD(sendMail:(NSDictionary *)obj resolver:(RCTPromiseResolveBlock)
 
     MCOAddress *replyTo = [MCOAddress addressWithDisplayName:nil
                                             mailbox:replyToAddress];
-
-    NSMutableArray *bccs = [[NSMutableArray alloc] init];
-    int bccCount = [bcc count];
-    for(int i = 0; i < bccCount; i++){
-          MCOAddress *newAddress = [MCOAddress addressWithMailbox:[bcc objectAtIndex:i]];
-          [bccs addObject:newAddress];
+    
+    if (bcc != nil) {
+        NSMutableArray *bccs = [[NSMutableArray alloc] init];
+        int bccCount = [bcc count];
+        for(int i = 0; i < bccCount; i++){
+              MCOAddress *newAddress = [MCOAddress addressWithMailbox:[bcc objectAtIndex:i]];
+              [bccs addObject:newAddress];
+        }
+        [[builder header] setBcc:bccs];
     }
 
     [[builder header] setFrom:from];
     [[builder header] setTo:@[to]];
-    [[builder header] setBcc:bccs];
     [[builder header] setReplyTo:@[replyTo]];
-
     [[builder header] setSubject:subject];
     [builder setHTMLBody:body];
-    int size = [attachmentPaths count];
-    for(int i = 0; i < size; i++){
-        [builder addAttachment:[MCOAttachment attachmentWithContentsOfFile:[attachmentPaths objectAtIndex:i]]];
+    if (attachmentPaths != nil && [attachmentPaths count] > 0) {
+        int size = [attachmentPaths count];
+          for(int i = 0; i < size; i++){
+              [builder addAttachment:[MCOAttachment attachmentWithContentsOfFile:[attachmentPaths objectAtIndex:i]]];
+          }
     }
     NSData * rfc822Data = [builder data];
     
     MCOSMTPSendOperation *sendOperation =
     [smtpSession sendOperationWithData:rfc822Data];
     [sendOperation start:^(NSError *error) {
-    if(error) {
-      NSLog(@"Error sending email: %@", error);
-      reject(@"Error",error.localizedDescription,error);
-    } else {
-      NSLog(@"Successfully sent email!");
-      NSDictionary *result = @{@"status": @"SUCCESS"};
-      resolve(result);
-    }
+      if (error) {
+        NSLog(@"Error sending email: %@", error);
+        reject(@"Error",error.localizedDescription,error);
+      } else {
+        NSLog(@"Successfully sent email!");
+        NSDictionary *result = @{@"status": @"SUCCESS"};
+        resolve(result);
+      }
     }];
 }
 
